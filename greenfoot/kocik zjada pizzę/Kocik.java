340b41b44b59a19d0238138c8f6cbf908d99a930
FanTir <31926994+FanTir4@users.noreply.github.com>
2017-11-10 09:20:06 +0100

    kocik druga wersja

--- greenfoot/kocik zjada pizzę/Kocik.java
+++ greenfoot/kocik zjada pizzę/Kocik.java
@@ -8,14 +8,61 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class Kocik extends Actor
 {
+    final int STAN_SIEDZI = 0;
+    final int STAN_W_PRAWO_1 = 1;
+    final int STAN_W_PRAWO_2 = 2;
+    final int STAN_W_LEWO_1 = 3;
+    final int STAN_W_LEWO_2 = 4;
+
+    int stanKocika = STAN_SIEDZI;
+
     public void ruch()
     {
-        if(Greenfoot.isKeyDown("right")) move(2);
-        if(Greenfoot.isKeyDown("left")) move(-2);
+        if(Greenfoot.isKeyDown("right"))
+        {
+            move(2);
+            if(stanKocika == STAN_W_PRAWO_1)
+                stanKocika = STAN_W_PRAWO_2;
+            else
+                stanKocika = STAN_W_PRAWO_1;
+        }
+        if(Greenfoot.isKeyDown("left")) 
+        {
+            move(-2);
+            if(stanKocika == STAN_W_LEWO_1)
+                stanKocika = STAN_W_LEWO_2;
+            else
+                stanKocika = STAN_W_LEWO_1;
+        }
+    }
+
+    public void zjadajPizze()
+    {
+        if(isTouching(Pizza.class))
+        {
+            removeTouching(Pizza.class);
+        }
+    }
+
+    public void ustawWyglad()
+    {
+        if(stanKocika == STAN_SIEDZI)
+            setImage("cat-sit.png");
+        if(stanKocika == STAN_W_PRAWO_1)
+            setImage("cat-walk-right.png");
+        if(stanKocika == STAN_W_PRAWO_2)
+            setImage("cat-walk-right2.png");
+        if(stanKocika == STAN_W_LEWO_1)
+            setImage("cat-walk-left.png");
+        if(stanKocika == STAN_W_LEWO_2)
+            setImage("cat-walk-left2.png");
+
     }
 
     public void act() 
     {
         ruch();
+        zjadajPizze();
+        ustawWyglad();
     }    
 }
