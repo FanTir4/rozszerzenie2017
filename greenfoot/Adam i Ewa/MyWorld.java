96d2c9778e5ac2fcc7dbbbc22e1b6413ecd245ab
FanTir <31926994+FanTir4@users.noreply.github.com>
2017-11-10 08:55:52 +0100

    Adam i Ewa dodanie węży

--- greenfoot/Adam i Ewa/MyWorld.java
+++ greenfoot/Adam i Ewa/MyWorld.java
@@ -28,15 +28,40 @@ public class MyWorld extends World
     {
         Drzewo drzewo = new Drzewo();
         addObject(drzewo,453,152);
-        
+
         Adam adam = new Adam();
         addObject(adam,54,100);
-        
+
         Ewa ewa = new Ewa();
         addObject(ewa,260,175);
-        
+
         Wychodek wychodek = new Wychodek();
         addObject(wychodek,72,361);
-        
+
+    }
+
+    public void act()
+    {
+        if(Greenfoot.isKeyDown("l"))
+        {
+            String odp = Greenfoot.ask("Ile węży?");
+            int ile = Integer.parseInt(odp);
+
+            int szerokosc = this.getWidth();
+            int wysokosc = this.getHeight();
+            for(int i=0; i <ile; i++)
+            {
+                int x = Greenfoot.getRandomNumber( szerokosc);
+                int y = Greenfoot.getRandomNumber( wysokosc);
+                Snake snake = new Snake();
+                addObject( snake, x, y);
+                while(snake.dotykaEwe() || snake.dotykaAdama())
+                {
+                    x = Greenfoot.getRandomNumber( szerokosc);
+                    y = Greenfoot.getRandomNumber( wysokosc);
+                    snake.setLocation(x, y);
+                }
+            }
+        }
     }
 }
