ac2d3c1e11443f488a1b9cf62fb1e4d5756a700e
FanTir <31926994+FanTir4@users.noreply.github.com>
2018-01-03 14:27:47 +0100

    eclipse

---
+++ eclipse/Start/src/Funkcje.java
@@ -0,0 +1,33 @@
+
+public class Funkcje {
+public static void  napis(String tekst)
+{
+	System.out.println("Przyj¹³em:" + tekst);
+}
+public static void  napis()
+{
+	System.out.println("Nic mi nie podano");
+}
+public static void  napis(String tekst1, String tekst2)
+{
+	System.out.println("Przyj¹³em:" + tekst1 + " oraz " + tekst2);
+}
+public static double policz( double x)
+{
+	x=x+8.0/19;
+	return x;
+		
+}
+	
+	public static void main(String[] args) {
+		double wynik = policz(12);
+		System.out.println("Po funkcji policz:" +wynik);
+		napis("indyk");
+		napis("kaczka");
+		napis();
+		napis("kaczka", "robot");
+	
+		
+	}
+
+}
